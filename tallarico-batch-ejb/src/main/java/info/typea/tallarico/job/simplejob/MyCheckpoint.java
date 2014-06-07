c3fe0104593e70be009d0fe5eab26c04a775ae22
pppiroto <pppiroto@gmail.com>
2014-06-07 18:22:11 +0900

    バッチREST作成前

--- tallarico-batch-ejb/src/main/java/info/typea/tallarico/job/simplejob/MyCheckpoint.java
+++ tallarico-batch-ejb/src/main/java/info/typea/tallarico/job/simplejob/MyCheckpoint.java
@@ -3,7 +3,7 @@ package info.typea.tallarico.job.simplejob;
 import java.io.Serializable;
 
 /**
- * 多くの場合、チャンク指向ステップのためにチェックポイントクラスを実装すべきです。以下のクラスは、テキストファイルの列番号を保持します。
+ * 多くの場合、チャンク指向ステップのためにチェックポイントクラスを実装すべきです。以下のクラスは、テキストファイルの行を保持します。
  * @see http://docs.oracle.com/javaee/7/tutorial/doc/batch-processing003.htm
  * @see http://kagamihoge.hatenablog.com/entry/2014/04/10/205918
  */
